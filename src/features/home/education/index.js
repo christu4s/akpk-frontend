702bbb560bd161a9ee449bacbfc68620abc1fb92
vendor_caspian01 - Admin <vendor_caspian01@akpk.org.my>
2022-08-29 16:09:22 +0800

    Connect and disconnected functionality updated with API integration

--- src/features/home/education/index.js
+++ src/features/home/education/index.js
@@ -6,7 +6,8 @@ import ChartCard from "../../../components/ChartCard";
 import ParticipationCard from "../../../components/ParticipationCard";
 import DashboardLayout from "../../../Layouts/dashboardLayout";
 import { useNavigate } from "react-router-dom";
-
+import { useAuth } from '../../../components/Auth';
+import axios from 'axios';
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: "20px 20px 5px 35px !important",
@@ -25,15 +26,34 @@ export default function Education() {
   const classes = useStyles();
   const navigate = useNavigate();
   const [connected, setConnected] = useState(false);
+  const {token, userInfo, user, setUserInfo} = useAuth();
   // const navigateClientLocation = event => {
   //     window.open('https://power.akpk.org.my/', '_blank', 'noopener,noreferrer');
   // }
-
-  const navigateClientLocation = event => {
+  console.log('i am new user',userInfo);
+ // setConnected(userInfo.fe_connected?userInfo.fe_connected:false);
+ 
+   const navigateClientLocation = event => {
     if(connected === false) {
-    window.open('http://10.250.1.121/osp-server/api/feconnect', '_self', 'noopener,noreferrer');
+       window.open('http://10.250.1.121/osp-server/api/feconnect', '_self', 'noopener,noreferrer');
     } 
   }
+
+ const navigateClientLocationDisconnect = event => {
+    
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    };
+    axios.get(`http://10.250.1.121/osp-server/api/disconnect_fe_portal`,config)
+    .then(result => {
+      console.log('result', result);
+      if(result.data.status == true) {
+        // setConnected(false);
+        setUserInfo({...userInfo, fe_connected: false});
+        // setConnected(false);
+      }
+    }) 
+ }
   
   return (
     <DashboardLayout>
@@ -44,10 +64,10 @@ export default function Education() {
             color="rgba(15, 90, 150, 0.6)"
             image="education"
             text="AKPK provides financial education programmes, materials and advisory on these matters relating to money management and proper use of credit; while offering modules that cater for specific financial needs in four (4) stages of life."
-            connected={connected}
+            connected={userInfo && userInfo.fe_connected}
             handleConnect={() => navigateClientLocation()}
             //handleConnect={() => setConnected(true)}
-           // handleDisonnect={() => setConnected(false)}
+            handleDisonnect={() => navigateClientLocationDisconnect()}
           />
         </Grid>
         <Grid container item spacing={2}>
