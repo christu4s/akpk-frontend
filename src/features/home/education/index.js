02bb185b2bd1377216a78e741b55cc75d576ace9
Khier Talia <gk_talia@esi.dz>
2022-09-05 23:54:47 +0100

    update education page

--- src/features/home/education/index.js
+++ src/features/home/education/index.js
@@ -8,6 +8,7 @@ import DashboardLayout from "../../../Layouts/dashboardLayout";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from '../../../components/Auth';
 import axios from 'axios';
+import DmpCard from "../../../components/DMPCard";
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: "20px 20px 5px 35px !important",
@@ -20,6 +21,21 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "25px !important",
     marginTop: "60px !important",
   },
+  button: {
+    outline: 'unset',
+    backgroundColor: '#06A8E5',
+    border: 'unset',
+    padding: '11px 16px',
+    color: '#FCFCFD',
+    borderRadius: 8,
+    fontSize: 16,
+    cursor: 'pointer',
+    
+  },
+  wrapper: {
+      padding: '80px 50px 240px 50px ',
+      justifyContent: 'center',
+  }
 }));
 
 export default function Education() {
@@ -68,72 +84,143 @@ export default function Education() {
             color="rgba(15, 90, 150, 0.6)"
             image="education"
             text="AKPK provides financial education programmes, materials and advisory on these matters relating to money management and proper use of credit; while offering modules that cater for specific financial needs in four (4) stages of life."
-            connected={userInfo && userInfo.fe_connected}
-            handleConnect={() => navigateClientLocation()}
-            //handleConnect={() => setConnected(true)}
-            handleDisonnect={() => navigateClientLocationDisconnect()}
+            // connected={userInfo && userInfo.fe_connected}
+            // handleConnect={() => navigateClientLocation()}
+            // //handleConnect={() => setConnected(true)}
+            // handleDisonnect={() => navigateClientLocationDisconnect()}
           />
         </Grid>
-        <Grid container item spacing={2}>
+        <div className={classes.wrapper}>
+        <Grid container item spacing={12} alignItems='center' justifyContent='center' >
+          <Grid item xs={6}>
+            <DmpCard
+              title='My Participation'
+              icon='participation'
+              content={[{
+                title: 'Learning',
+                value: '1'
+              },
+              {
+                title: 'Event',
+                value: '1'
+              },
+              {
+                title: 'Research',
+                value: '1'
+              },
+              {
+                title: 'Publication',
+                value: '1'
+              },
+            ]}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <DmpCard
+              title='My Contributions'
+              icon='contributions'
+              content={[{
+                title: 'Downloads',
+                value: '1'
+              },
+              {
+                title: 'Likes',
+                value: '1'
+              },
+              {
+                title: 'Shares',
+                value: '1'
+              },
+            ]}
+            />
+          </Grid>
           <Grid item xs={12}>
-            <Typography className={classes.participation}>
-              My Participation
-            </Typography>
+            <DmpCard
+              title='My Transactions'
+              icon='transactions'
+              Button={<button className={classes.button}>View transactions</button>}
+              content={[{
+                title: 'Learning',
+                value: 'RM 0.00'
+              },
+              {
+                title: 'Event',
+                value: 'RM 0.00'
+              },
+              {
+                title: 'Publication',
+                value: 'RM 0.00'
+              },
+              {
+                title: 'Publication',
+                value: 'RM 0.00'
+              },
+            ]}
+            />
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-        </Grid>
-        <Grid container item spacing={2}>
           <Grid item xs={12}>
-            <Typography className={classes.participation}>
-              My Purchases
-            </Typography>
+            <DmpCard
+              title='My Badges'
+              icon='badges'
+              Button={<button className={classes.button}>View badges</button>}
+              content={[{
+                title: 'Login Badge',
+                value: '0'
+              },
+              {
+                title: 'Learning Badge',
+                value: '0'
+              },
+              {
+                title: 'Test Badge',
+                value: '0'
+              },
+              {
+                title: 'Communication Badge',
+                value: '0'
+              },
+            ]}
+            />
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ChartCard />
+          <Grid item xs={6}>
+            <DmpCard
+              title='My Achievements'
+              icon='achievements'
+              Button={<button className={classes.button}>View point</button>}
+               content={[{
+                title: 'Accumulated Points',
+                value: '0'
+              },
+              {
+                title: 'Level',
+                value: '0'
+              },
+              
+            ]}
+            />
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ChartCard />
+          <Grid item xs={6}>
+            <DmpCard
+              title='My Series'
+              icon='series'
+              Button={<button className={classes.button}>View series</button>}
+              content={[{
+                title: 'Your Recent Series'
+              }]}
+            />
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ChartCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ChartCard />
-          </Grid>
-        </Grid>
-        <Grid container item spacing={2}>
           <Grid item xs={12}>
-            <Typography className={classes.participation}>
-              My Courses
-            </Typography>
+            <DmpCard
+              title='My Courses'
+              icon='courses'
+              Button={<button className={classes.button}>View courses</button>}
+              content={[{
+                title: '1. Templat Praktikal Pengurusan Kewangan PKS - 30/08/2022'
+              }]}
+            />
           </Grid>
-          {Object.values(courses).map(({ courseTitle, courseDate }) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-              <CourseCard course_title={{courseTitle}} course_date={{courseDate}} />
-            </Grid>
-           ))}
-          
-          {/* <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
-            <ParticipationCard />
-          </Grid> */}
-        </Grid>
+         </Grid>
+        </div>
       </Grid>
     </DashboardLayout>
   );
